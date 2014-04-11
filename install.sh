37131fc739c5de0a8a0fd5c6917baa17f6e76872
Donato Borrello <venar303@gmail.com>
2014-04-11 10:50:53 -0400

    Commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+
+## Install yadr-ubuntu
+
+## Link up the dotfiles
+ln -s ~/.bashrc ~/dotfiles/.bashrc
+ln -s ~/.dotfiles/update_paths.zsh  ~/.zsh.after/update_paths.zsh
+ln -s ~/.dotfiles/.vimrc.after  ~/.vimrc.after
+ln -s ~/.dotfiles/tweaks.zsh  ~/.zsh.after/tweaks.zsh
+ln -s ~/.yadr/fonts ~/.fonts
+
+
+.jshintrc
+.prompt
+.tmux.conf
+.hgrc
+.gitconifg
+.bash_aliases
+
+Inconsolata XL 12,Inconsolata 15,Monaco 12 
